cc249e279e02514237b48aaef55be523164e40ca
Fakhri <fakhri.aunur@gmail.com>
2024-10-24 15:24:32 +0700

    Add REPL

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exiting the Pokedex...")
+	os.Exit(0)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		if command, exists := commands[input]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+		} else {
+			fmt.Println("Unknown comman. Type help for a list of available commands.")
+		}
+	}
 }
